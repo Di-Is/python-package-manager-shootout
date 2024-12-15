7efa369cf4b8b5ac3e6af93847c30ad51b56dad5
Di-Is <hommageMe@gmail.com>
2024-12-15 17:32:03 +0900

    f

--- bin/actions_prereqs.sh
+++ bin/actions_prereqs.sh
@@ -3,7 +3,9 @@ set -euf -o pipefail
 
 # sentry dependencies
 sudo apt-get update -qq
-sudo apt-get install -y libxmlsec1-dev librdkafka-dev hyperfine
+sudo apt-get install -y libxmlsec1-dev librdkafka-dev
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.19.0/hyperfine_1.19.0_amd64.deb
+sudo dpkg -i hyperfine_1.19.0_amd64.deb
 # benchmark setup
 pip --disable-pip-version-check --no-cache-dir install csv2md
 mkdir -p timings
