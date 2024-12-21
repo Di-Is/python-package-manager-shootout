f0215717f67c9fb90476efdbeb8bf081adc58304
Di-Is <hommageMe@gmail.com>
2024-12-22 01:25:55 +0900

    f

--- benchmark.py
+++ benchmark.py
@@ -218,6 +218,14 @@ def cli(args: Args) -> None:
                     shutil.copy2(src_path, f"{temp_dir}/{item}")
 
         del os.environ["VIRTUAL_ENV"]
+        if "uv/archive-v0" in os.environ["PATH"]:
+            os.environ["PATH"] = ":".join(
+                [
+                    item
+                    for item in os.environ["PATH"].split(":")
+                    if "uv/archive-v0" not in item
+                ]
+            )
         subprocess.run(
             [
                 "hyperfine",
